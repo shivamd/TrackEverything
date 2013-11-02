b372ed7df1b79c55d1b90b0cc3d2397f351bdd04
Shivam Daryanani <daryananis@gmail.com>
2013-11-02 11:55:15 +0400

    Model for topic and constanct for frequencies

---
+++ app/models/topic.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: topics
+#
+#  id        :integer          not null, primary key
+#  name      :string(255)
+#  frequency :string(255)
+#
+
+class Topic < ActiveRecord::Base
+  FREQUENCIES = %w(
+    daily
+    weekdays
+    sunday
+    monday
+    tuesday
+    wednesday
+    thursday
+    friday
+    saturday
+    monthly
+  )
+  validates :name, :frequency, presence: true
+end
