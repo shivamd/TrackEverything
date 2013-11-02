700807867cbdb1162c109935e6923757d3092d65
Shivam Daryanani <daryananis@gmail.com>
2013-11-02 13:21:58 +0400

    Form and creation for topics

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,24 @@
+class TopicsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def new 
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    unless @topic.save 
+      flash.now[:error] = "Unable to create topic"
+      render :new
+    else
+      UsersTopic.create(topic_id: @topic.id, user_id: current_user.id)
+      redirect_to root_path, notice: "Successfully created a topic"
+    end
+  end
+
+  private 
+
+  def topic_params
+    params.require(:topic).permit(:frequency, :name)
+  end
+end
