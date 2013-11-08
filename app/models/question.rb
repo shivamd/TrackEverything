304e1ef1595b58b3d55fa72f1554872cb58683e9
Shivam Daryanani <daryananis@gmail.com>
2013-11-09 00:41:39 +0400

    Question model relationship with answers and updated spec

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
   belongs_to :topic, inverse_of: :questions
 
   validates :content, :topic,  presence: true
+
+  has_many :answers
+  def create_answer(answer)
+    self.answers.create(answer: answer) 
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Question do 
   it { should belong_to(:topic) }
   it { should validate_presence_of(:content) }
-  it { should validate_presence_of(:topic_id) }
+  it { should validate_presence_of(:topic) }
+  it { should have_many(:answers) }
 end
