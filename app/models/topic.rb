66eb7e5564d61273a315bea8f87fd54b86e59a91
Shivam Daryanani <daryananis@gmail.com>
2013-11-09 01:17:30 +0400

    Code to receive email and create answers
    It's commented out because we still haven't implemented the NameCheap API in order to create emails for the domain on the fly

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -28,4 +28,21 @@ class Topic < ActiveRecord::Base
 
   has_many :questions, inverse_of: :topic
   accepts_nested_attributes_for :questions, allow_destroy: true
+
+  def self.receive_mail(email,answers)
+    # find the topic and do all the email stuff
+    # users_email = email.from
+    # id = users_email.match(//)
+    # topic = Topic.find_by_id(id)
+    # user = User.find_by_email(users_email)
+    # if topic.has_user?(user)
+    #   topic.questions.each_with_index do |question, i|
+    #     question.create_answer(answers[i])
+    #   end
+    # end
+  end
+
+  def has_user?(user)
+    topic.users.include?(user)
+  end
 end
