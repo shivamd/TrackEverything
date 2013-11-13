de9707856524a15c0c6be23eca1baf3f9e424727
Shivam Daryanani <daryananis@gmail.com>
2013-11-13 12:45:54 +0400

    Created class that processes emails and creates the associated answers for each question

---
+++ app/models/answer_via_email.rb
@@ -0,0 +1,46 @@
+class AnswerViaEmail
+
+  def self.process(email)
+    new(email).process
+  end
+
+  def initialize(email)
+    @email = email
+  end
+
+  def process
+    create_answers if user_has_topic?
+  end
+
+  private
+
+  def create_answers
+    questions.each_with_index do |question, i|
+      create_answer(answers[i], question)
+    end
+  end
+
+  def create_answer(answer, question)
+    Answer.create(content: answer, user_id: user.id, question_id: question.id)
+  end
+
+  def questions
+    topic.questions
+  end
+
+  def answers
+   @email.body.split(/\n+/)
+  end
+
+  def user_has_topic?
+    topic.has_user?(user) if topic
+  end
+
+  def topic
+    Topic.find_by_name(@email.to.first[:token])
+  end
+
+  def user 
+    User.find_by_email(@email.from)
+  end
+end
