70df002ed825245ad6083a8637b13e5c44faae2e
Shivam Daryanani <daryananis@gmail.com>
2013-11-13 10:44:24 +0400

    Replace placeholder logic to create answers for each question in a topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -29,20 +29,20 @@ class Topic < ActiveRecord::Base
   has_many :questions, inverse_of: :topic
   accepts_nested_attributes_for :questions, allow_destroy: true
 
-  def self.receive_mail(email,answers)
-    # find the topic and do all the email stuff
-    # users_email = email.from
-    # id = users_email.match(//)
-    # topic = Topic.find_by_id(id)
-    # user = User.find_by_email(users_email)
-    # if topic.has_user?(user)
-    #   topic.questions.each_with_index do |question, i|
-    #     question.create_answer(answers[i])
-    #   end
-    # end
+  def self.receive_email(email)
+    user = User.find_by_email(email.from)
+    if user
+      answers = email.body.split(/\r\n\r\n/)
+      topic = Topic.find_by_name(email.to.first[:name])
+      if topic && topic.has_user?(user)
+        topic.questions.each_with_index do |question, i|
+          question.create_answer(answers[i])
+        end
+      end
+    end
   end
 
   def has_user?(user)
-    topic.users.include?(user)
+    self.users.include?(user)
   end
 end
