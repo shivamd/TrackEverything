ff7ec4d3dbc3b88671dca1bfdbead8787bdd5a7c
Shivam Daryanani <daryananis@gmail.com>
2013-11-24 00:58:23 +0400

    User sees question item test

--- spec/integration/manaage_topics_spec.rb
+++ spec/integration/manaage_topics_spec.rb
@@ -17,11 +17,22 @@ feature "manage topics" do
     user_does_not_see_topic_item "hello"
   end
 
+  scenario "questions should belong to topic", js: true do
+    sign_up
+    create_topic_with_questions "how are you?", "What did you get done today?"
+    user_sees_question_item "how are you?"
+    user_sees_question_item "how are you?"
+  end
+
   def user_sees_topic_item(description)
     expect(page).to have_css "h2", text: description
   end
   
   def user_does_not_see_topic_item(description)
     expect(page).to_not have_css "h2", text: description
+  end
+
+  def user_sees_question_item(question)
+    expect(page).to have_css "li", text: question
   end
 end
--- spec/support/topics_helper.rb
+++ spec/support/topics_helper.rb
@@ -6,6 +6,21 @@ module TopicsHelper
     fill_in "Question", with: "Why am I here?"
     click_button "Create topic"
   end
+
+  def create_topic_with_questions *questions
+    click_link "Add Topic"
+    fill_in "Name", with: "My Topic"
+    find(:css, "input[id='topic_time']").set "6:00pm"
+    fill_in "Question", with: questions[0]
+    questions[1..-1].each do |question|
+    click_link "Add Question"
+      last_nested_fields = all(".question").last
+      within(last_nested_fields) do
+        fill_in "Question", with: question
+      end
+    end
+    click_button "Create topic"
+  end
 end
 
 RSpec.configure do |config|
