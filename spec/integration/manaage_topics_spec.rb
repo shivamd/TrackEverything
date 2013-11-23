1efe42d671a6e4d816c7c5cfe08727b085c41220
Shivam Daryanani <daryananis@gmail.com>
2013-11-23 20:38:36 +0400

    Test to ensure a user only views their topics

--- spec/integration/manaage_topics_spec.rb
+++ spec/integration/manaage_topics_spec.rb
@@ -12,4 +12,17 @@ feature "manage topics" do
     expect(page).to have_css "h2", text: "My Topic"
     expect(page).to have_css "li", text: "Why am I here?"
   end
+
+  scenario "view only my topics" do 
+    Topic.create(name: "hello")
+    sign_up
+    click_link "Add Topic"
+    fill_in "Name", with: "My Topic"
+    find(:css, "input[id='topic_time']").set "6:00pm"
+    fill_in "Question", with: "Why am I here?"
+    click_button "Create topic"
+
+    expect(page).to have_css "h2", text: "My Topic"
+    expect(page).to_not have_css "h2", text: "hello"
+  end
 end
